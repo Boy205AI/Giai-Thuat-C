d1781f2bffede6bb04eabeb0f1d1b5db960bf35d
Cậu Bé IT <Nhatlx205@gmail.com>
2026-04-17 04:45:46 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+#define nl '\n'
+#define ll long long 
+const ll MOD = 1e9 + 7;
 
 int main() {
-    cout << "Moi truong UCRT64 Portable tren Github da san sang!" << endl;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    cout << "Moi truong g++ da san sang!" << nl;
     return 0;
 }
