7d445bf6ebe0dc00a8a1a0e2a0d5361391b14ede
Cậu Bé IT <Nhatlx205@gmail.com>
2026-05-21 23:08:40 +0700

    Update and rename install_vscode_msys.bat to install_vscode.bat

--- install_vscode.bat
+++ install_vscode.bat
@@ -6,3 +6,4 @@ Write-Host "=== ĐANG CÀI ĐẶT VISUAL STUDIO CODE ===" -ForegroundColor Green
 winget install -e --id Microsoft.VisualStudioCode --accept-package-agreements --accept-source-agreements --silent
 
 Write-Host "Cài đặt Visual Studio Code hoàn tất!" -ForegroundColor Cyan
+ 
